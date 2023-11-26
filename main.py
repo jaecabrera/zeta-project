0efdec2d26843801a2ac6399356088dc7ded10b9
jaeca <jaecabrera.cp@gmail.com>
2023-11-27 01:34:15 +0800

    - created movement manager on main.py - create agent sprite class for main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import logging
 from pathlib import Path
-import numpy as np
 import pyglet as pyg
 from pyglet.window import key, mouse
+from dataclasses import dataclass
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -17,44 +17,80 @@ window = pyg.window.Window(width=window_width, height=window_height, fullscreen=
 fps_display = pyg.window.FPSDisplay(window=window)
 
 """ Sprite Sheet """
-agent_idle = Path.cwd() / "assets" / "sprite" / "1 Old_man" / "Old_man_walk.png"
+agent_idle = Path.cwd() / "assets" / "sprite" / "4 Woman" / "Woman_walk.png"
 sprite_sheet = pyg.image.load(agent_idle)
-
 num_rows = 1
 num_cols = 6
-
 frame_width = sprite_sheet.width // num_cols
 frame_height = sprite_sheet.height // num_rows
+animation = pyg.image.ImageGrid(sprite_sheet, num_rows, num_cols)
+texture_grid = pyg.image.TextureGrid(animation)
+sprite = pyg.sprite.Sprite(texture_grid[0])
 
 logging.debug(f"Agent: sheet-width {sprite_sheet.width}, frame-height {sprite_sheet.height}")
 logging.debug(f"Agent: frame-width {frame_width}, frame-height {frame_height}")
 
-speed = 160
-animation = pyg.image.ImageGrid(sprite_sheet, num_rows, num_cols)
-texture_sequence = pyg.image.TextureGrid(animation)
-logging.debug(texture_sequence[0])
-sprite = pyg.sprite.Sprite(texture_sequence[0])
-logging.debug(f"sequence: {len(texture_sequence)}")
+speed = 90
 
-sprite.x = window_width // 2 - frame_width // 2
-sprite.y = window_height // 2 - frame_height // 2
-
-is_moving_left = False
-is_moving_right = False
+""" Starting Position """
+agent_pos_x = window_width // 2 - frame_width // 2
+agent_pos_y = window_height // 2 - frame_height // 2
 
 
-# Define the update function to handle sprite animation and movement
+@dataclass
+class MovementManager:
+    left: bool = False
+    right: bool = False
+    up: bool = False
+    down: bool = False
+
+    def move(self, direction: str, state: bool):
+        match direction:
+            case 'left':
+                self.left = state
+            case 'up':
+                self.up = state
+            case 'down':
+                self.down = state
+            case 'right':
+                self.right = state
+
+
+class Agent(pyg.sprite.Sprite, MovementManager):
+
+    def __init__(self, sprite_grid, x, y, spd):
+        super(Agent, self).__init__(sprite_grid[0], x, y)
+        self.sprite_grid = sprite_grid
+        self.current_frame = 0
+        self.speed = spd
+
+    def update(self, dt):
+
+        self.image = texture_grid[self.current_frame]
+
+        if self.left:
+            self.x -= speed * dt
+            self.current_frame = (self.current_frame + 1) % len(texture_grid)
+
+        if self.right:
+            self.x += speed * dt
+            self.current_frame = (self.current_frame + 1) % len(texture_grid)
+
+        if self.up:
+            self.y += speed * dt
+            self.current_frame = (self.current_frame + 1) % len(texture_grid)
+
+        if self.down:
+            self.y -= speed * dt
+            self.current_frame = (self.current_frame + 1) % len(texture_grid)
+
+
+""" Agent Initialized """
+agent = Agent(texture_grid, agent_pos_x, agent_pos_y, speed)
+
+
 def update(dt):
-    global is_moving_left
-
-    # Update the sprite animation based on the elapsed time
-    sprite.image = texture_sequence[3]
-
-    if is_moving_left:
-        sprite.x -= speed * dt
-
-    if is_moving_right:
-        sprite.x += speed * dt
+    agent.update(dt)
 
 
 # Schedule the update function to be called regularly
@@ -65,46 +101,43 @@ pyg.clock.schedule_interval(update, 1 / 60.0)
 def on_draw():
     window.clear()
     fps_display.draw()
-    sprite.draw()
+    agent.draw()
 
 
 @window.event
 def on_key_press(symbol, modifiers):
-    global is_moving_left
-    global is_moving_right
-
     match symbol:
 
-        case key.A:
-            for x in np.arange(1, len(texture_sequence[1:])):
-                sprite.image = texture_sequence[x]
-                is_moving_left = True
-
         case key.W:
-            print('pressed W')
+            agent.move('up', True)
+
+        case key.A:
+            agent.move('left', True)
 
         case key.S:
-            print('pressed S')
+            agent.move('down', True)
 
         case key.D:
-            sprite.image = texture_sequence[3]
-            is_moving_right = True
+            agent.move('right', True)
 
 
 @window.event
 def on_key_release(symbol, modifiers):
-    global is_moving_left
-    global is_moving_right
-
     match symbol:
-        case key.A:
-            is_moving_left = False
+
         case key.W:
-            print('pressed W')
+            agent.move('up', False)
+
+        case key.A:
+            agent.move('left', False)
+
         case key.S:
-            print('pressed S')
+            agent.move('down', False)
+
         case key.D:
-            is_moving_right = False
+            agent.move('right', False)
+
+    agent.current_frame = 0
 
 
 @window.event
